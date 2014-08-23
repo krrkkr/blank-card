7b4ac01b0f9860f7df580f1a409000785b9318b6
김동현 <dh047@naver.com>
2014-08-23 17:12:28 +0900

    add facebook API

---
+++ facebook/loginToFacebook.js
@@ -0,0 +1,92 @@
+﻿
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '327186377450225',
+        cookie: true,  // enable cookies to allow the server to access the session
+        xfbml: true,  // parse social plugins on this page
+        version: 'v2.0' // use version 2.0
+    });
+
+    // Now that we've initialized the JavaScript SDK, we call 
+    // FB.getLoginStatus().  This function gets the state of the
+    // person visiting this page and can return one of three states to
+    // the callback you provide.  They can be:
+    //
+    // 1. Logged into your app ('connected')
+    // 2. Logged into Facebook, but not your app ('not_authorized')
+    // 3. Not logged into Facebook and can't tell if they are logged into
+    //    your app or not.
+    //
+    // These three cases are handled in the callback function.
+
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+};
+
+// Load the SDK asynchronously
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/ko_KR/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        testAPI();
+    } else if (response.status === 'not_authorized') {
+        // The person is logged into Facebook, but not your app.
+        document.getElementById('status').innerHTML = 'Please log ' + 'into this app.';
+    } else {
+        // The person is not logged into Facebook, so we're not sure if
+        // they are logged into this app or not.
+        document.getElementById('status').innerHTML = 'Please log ' + 'into Facebook.';
+    }
+}
+
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+}
+
+function getFriends() {
+    FB.getLoginStatus(handleSessionResponse);
+    function handleSessionResponse(response) {
+        if (!response.session) {
+            //
+            document.write("dsf");
+        } else {
+            document.write("dsf");
+            FB.api(
+             {
+                 method: 'fql.query',
+                 query: 'SELECT uid, name, email, birthday, pic_square, online_presence FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())'
+             },
+            function (response) {
+                for (var i = 0; i < response.length; i++) {
+                    document.write(response[i].uid);
+                    response[i].pic_square;
+                    response[i].name;
+                    response[i].birthday;
+                    response[i].email;
+                    response[i].online_presence;
+                }
+            }
+            );
+        }
+    }
+}
